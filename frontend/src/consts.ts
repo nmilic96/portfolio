002553a40fc8a2327bb7290a009116d7b2546f39
nmilic96 <milic.n96@gmail.com>
2024-01-21 17:43:28 +0100

    Add new files and update configurations

---
+++ frontend/src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Nikola Milić';
+export const SITE_DESCRIPTION = 'Frontend web developer based in Đakovo, Croatia.';

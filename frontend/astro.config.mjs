2c6cde48b2cb2dd1ac79b31e55435ec08ff86967
nmilic96 <milic.n96@gmail.com>
2024-01-21 23:14:06 +0100

    Add base path to Astro config

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -9,5 +9,6 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://nmilic96.github.io',
+  base: 'portfolio-new/frontend',
   integrations: [mdx(), sitemap(), tailwind(), icon(), react()]
 });

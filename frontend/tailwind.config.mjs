8576013a2a97236744f5875a4895524a529e2c44
nmilic96 <milic.n96@gmail.com>
2024-01-22 10:19:52 +0100

    Update FloatingHead and global styles

--- frontend/tailwind.config.mjs
+++ frontend/tailwind.config.mjs
@@ -2,6 +2,21 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		screens: {
+			'sm': '640px',
+			// => @media (min-width: 640px) { ... }
+
+			'md': '768px',
+			// => @media (min-width: 768px) { ... }
+
+			'lg': '1024px',
+			// => @media (min-width: 1024px) { ... }
+
+			'xl': '1280px',
+			// => @media (min-width: 1280px) { ... }
+
+			'2xl': '1536px'
+		},
 		extend: {},
 	},
 	plugins: [],

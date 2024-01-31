ab098dfd622193ff1e162cbe0b2279941c9d090e
nmilic96 <milic.n96@gmail.com>
2024-01-31 19:41:23 +0100

    Add partytown integration to astro.config.mjs

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -3,15 +3,16 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
-
 import react from "@astrojs/react";
+
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://nmilic96.github.io',
   base: '/portfolio',
   buildOptions: {
-    out: './dist',  // output directory
+    out: './dist' // output directory
   },
-  integrations: [mdx(), sitemap(), tailwind(), icon(), react()]
+  integrations: [mdx(), sitemap(), tailwind(), icon(), react(), partytown()]
 });
